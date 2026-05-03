81e27ebb6871514c071ff4fc897ff89597760803
kywoo26 <kywoo26@naver.com>
2026-05-03 20:30:36 +0900

    Revert "style(compiler): consolidate schema imports in taxonomy.py"
    This reverts commit d35704ea08048f62ecb005db885268f2be9a791a.

--- src/snowiki/compiler/taxonomy.py
+++ src/snowiki/compiler/taxonomy.py
@@ -5,8 +5,14 @@ from typing import Any
 
 from snowiki.schema.compiled import (
     CompiledPage as _CompiledPage,
+)
+from snowiki.schema.compiled import (
     PageSection as _PageSection,
+)
+from snowiki.schema.compiled import (
     PageType as _PageType,
+)
+from snowiki.schema.compiled import (
     compiled_page_path as _compiled_page_path,
 )
 from snowiki.schema.normalized import NormalizedRecord as _NormalizedRecord
